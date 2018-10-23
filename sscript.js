a53b5683685884f8c6f2c8dfe04ded648c40760b
Sagid Magomedov <sagid-m@yandex-team.ru>
2018-10-23 14:02:45 +0300

    Update sscript.js using later ES and format a bit

--- sscript.js
+++ sscript.js
@@ -1,19 +1,20 @@
-(() =>{
-	let tabLinks = document.querySelectorAll('.tab__link');
-	let	tabContentItems = document.querySelectorAll('.tab__item');
-	let	tabList = document.querySelector('#tab_list');
+(() => {
+	const tabLinks = document.querySelectorAll('.tab__link');
+	const tabContentItems = document.querySelectorAll('.tab__item');
+	const tabList = document.querySelector('#tab_list');
 
 	tabLinks[0].classList.add('active_tab');
-	tabList.addEventListener('click', (e) =>{
-		for(let i = 0; i < tabLinks.length; i++){
-			if (tabLinks[i] != e.target) {
-				tabLinks[i].classList.remove('active_tab');
-				tabContentItems[i].classList.remove('active_tab-content');
+	tabList.addEventListener('click', (e) => {
+		tabLinks.forEach((tabLink, i) => {
+			const tabContentItem = tabContentItems[i];
+			if (tabLink === e.target) {
+				tabLink.classList.add('active_tab');
+				tabContentItem.classList.add('active_tab-content');
 			}
-			else{
-				tabLinks[i].classList.add('active_tab');
-				tabContentItems[i].classList.add('active_tab-content')
+			else {
+				tabLink.classList.remove('active_tab');
+				tabContentItem.classList.remove('active_tab-content');
 			}
-		}
-	})
+		});
+	});
 })();
